0ee1aee15e2380f15ccd57cededd7ea7bcccd133
Kim van Wyk <kimv@sahomeloans.com>
2019-01-17 13:52:52 +0200

    Add ltablex support

--- latex-container/setup.sh
+++ latex-container/setup.sh
@@ -1,2 +1,2 @@
 tlmgr init-usertree
-tlmgr install savetrees titlesec ucs multirow breakurl ulem footmisc microtype fancyhdr mptopdf lastpage sectsty
+tlmgr install savetrees titlesec ucs multirow breakurl ulem footmisc microtype fancyhdr mptopdf lastpage sectsty tools ltablex
