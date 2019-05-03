8ec2406441e277cf3806d418123f8e2abe62fd92
Kim van Wyk <kimv@sahomeloans.com>
2019-05-03 17:34:50 +0200

    Getting latest texlive install working

--- latex/setup.sh
+++ latex/setup.sh
@@ -1,3 +1,2 @@
-tlmgr option repository ftp://tug.org/historic/systems/texlive/2019/tlnet-final
 tlmgr init-usertree
 tlmgr install savetrees titlesec ucs multirow breakurl ulem footmisc microtype fancyhdr mptopdf lastpage sectsty tools ltablex
