ef8e75aa35e2c51a5e7bd421b700aac19933aff4
Kim van Wyk <kimv@sahomeloans.com>
2019-05-03 15:35:19 +0200

    Set to 2019 value

--- latex/setup.sh
+++ latex/setup.sh
@@ -1,2 +1,3 @@
+tlmgr option repository ftp://tug.org/historic/systems/texlive/2019/tlnet-final
 tlmgr init-usertree
 tlmgr install savetrees titlesec ucs multirow breakurl ulem footmisc microtype fancyhdr mptopdf lastpage sectsty tools ltablex
