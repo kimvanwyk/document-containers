237302929c1f24f1b31ab4bfe80f6d71c8a38a31
kimv <kimv@sahomeloans.com>
2021-10-29 21:08:58 +0200

    Add xcolor and use ubuntu 20.04

--- latex/setup.sh
+++ latex/setup.sh
@@ -1,2 +1,2 @@
 tlmgr init-usertree
-tlmgr install savetrees titlesec ucs multirow breakurl ulem footmisc microtype fancyhdr mptopdf lastpage sectsty tools ltablex collection-fontsrecommended xkeyval booktabs parskip etoolbox
+tlmgr install savetrees titlesec ucs multirow breakurl ulem footmisc microtype fancyhdr mptopdf lastpage sectsty tools ltablex collection-fontsrecommended xkeyval booktabs parskip etoolbox xcolor
