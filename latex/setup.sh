17d9e46d2a9e29cbc6de4638a69d1bf59144bc4f
kimv <kimv@sahomeloans.com>
2022-10-09 16:49:02 +0200

    Add Beamer support

--- latex/setup.sh
+++ latex/setup.sh
@@ -1,2 +1,3 @@
 tlmgr init-usertree
-tlmgr install savetrees titlesec ucs multirow breakurl ulem footmisc microtype fancyhdr mptopdf lastpage sectsty tools ltablex collection-fontsrecommended xkeyval booktabs parskip etoolbox xcolor
+tlmgr update --self
+tlmgr install savetrees titlesec ucs multirow breakurl ulem footmisc microtype fancyhdr mptopdf lastpage sectsty tools ltablex collection-fontsrecommended xkeyval booktabs parskip etoolbox xcolor beamer pgf
